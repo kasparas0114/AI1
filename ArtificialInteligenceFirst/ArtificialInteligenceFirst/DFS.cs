c137efcd44f50da438f0c9812599471d1800b478
Vytautas <atgrupe@gmail.com>
2016-03-12 13:51:24 +0000

    veikia bfs

--- ArtificialInteligenceFirst/ArtificialInteligenceFirst/DFS.cs
+++ ArtificialInteligenceFirst/ArtificialInteligenceFirst/DFS.cs
@@ -6,14 +6,8 @@ using System.Threading.Tasks;
 
 namespace ArtificialInteligenceFirst
 {
-    class DFS
+    class DFS:SearchAlgorithms
     {
-
-        List<GraphNode> nodesList = new List<GraphNode>();
-        public int[,] startMatrix = new int[3, 3];
-        public int[,] correctMatrix = new int[3, 3] { {1, 2, 3 }, { 8, 0, 4 },
-                                        {7,6, 5} };
-        Graph newGraph;
         public DFS()
         {
             newGraph = new Graph();
@@ -25,7 +19,7 @@ namespace ArtificialInteligenceFirst
             bool k = true;
             int[,] startMatrix = ((new CreateNodeConsole()).GetNode()).NumbersArray;
             nodesList.Add(new GraphNode(startMatrix));
-            
+
             while (k)
             {
                 List<GraphNode> tempList = nodesList.ToList();
@@ -35,9 +29,9 @@ namespace ArtificialInteligenceFirst
                 }
                 foreach (GraphNode item in nodesList)
                 {
-                    if(compareArrays(correctMatrix, item.NumbersArray))
+                    if (compareArrays(correctMatrix, item.NumbersArray))
                     {
-                        Console.WriteLine("Radau");
+                        Console.WriteLine("Found");
                         k = false;
                         PrintPath(item);
                     }
@@ -45,110 +39,9 @@ namespace ArtificialInteligenceFirst
             }
             Console.ReadLine();
         }
-        public void PrintPath(GraphNode node)
-        {
-            while (node != null)
-            {
-                printNode(node);
-                node = node.backElement;
-            }
-        }
-        public void printNode(GraphNode node)
-        {
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    Console.Write(node.NumbersArray[i, j] + " ");   
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine();
-        }
         public void Move(GraphNode node)
         {
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    if (node.NumbersArray[i, j] == 0)
-                    {
-                        if (i < 2) MoveRight(node, i, j);
-                        if (i > 0) MoveLeft(node, i, j);
-                        if (j < 2) MoveDown(node, i, j);
-                        if (j > 0) MoveUp(node, i, j);
-                    }
-                }
-                nodesList.Remove(node);
-            }
-        }
-        public bool compareArrays(int[,] arrayA, int[,] arrayB)
-        {
-            int counter = 0;
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    if (arrayA[i, j] == arrayB[i, j])
-                    {
-                        counter++;
-                    }
-                }
-            }
-            if (counter == 9)
-            {
-                return true;
-            }
-            else return false;
-        }
-        public void MoveUp(GraphNode node, int row, int line)
-        {
-            int[,] matrix = node.GetNodeMatrix();
-            int[,] tempMatrix = (int[,])matrix.Clone();
-            tempMatrix[row, line] = tempMatrix[row, line-1];
-            tempMatrix[row, line-1] = 0;
-            GraphNode newItem = new GraphNode(tempMatrix);         
-            newGraph.addNode(newItem);
-            newGraph.addEdge(node, newItem);
-            nodesList.Add(newItem);
-
-            
-        }
-        public void MoveDown(GraphNode node, int row, int line)
-        {
-            int[,] matrix = node.GetNodeMatrix();
-            int[,] tempMatrix = (int[,])matrix.Clone();
-            tempMatrix[row, line] = tempMatrix[row , line+1];
-            tempMatrix[row , line+1] = 0;
-            GraphNode newItem = new GraphNode(tempMatrix);
-            
-            newGraph.addNode(newItem);
-            nodesList.Add(newItem);
-            newGraph.addEdge(node, newItem);
-        }
-        public void MoveLeft(GraphNode node, int row, int line)
-        {
-            int[,] matrix = node.GetNodeMatrix();
-            int[,] tempMatrix = (int[,])matrix.Clone();
-            tempMatrix[row, line] = tempMatrix[row-1, line];
-            tempMatrix[row-1, line] = 0;
-            GraphNode newItem = new GraphNode(tempMatrix);
-            
-            newGraph.addNode(newItem);
-            newGraph.addEdge(node, newItem);
-            nodesList.Add(newItem);
-        }
-        public void MoveRight(GraphNode node, int row, int line)
-        {
-            int[,] matrix = node.GetNodeMatrix();
-            int[,] tempMatrix = (int[,])matrix.Clone();
-            tempMatrix[row, line] = tempMatrix[row+1, line];
-            tempMatrix[row+1, line] = 0;
-            GraphNode newItem = new GraphNode(tempMatrix);
-          
-            newGraph.addNode(newItem);
-            newGraph.addEdge(node, newItem);
-            nodesList.Add(newItem);
+           
         }
     }
 }
---
+++ ArtificialInteligenceFirst/ArtificialInteligenceFirst/SearchAlgorithms.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtificialInteligenceFirst
+{
+    abstract  class SearchAlgorithms
+    {
+      
+        public List<GraphNode> nodesList = new List<GraphNode>();
+        public int[,] startMatrix = new int[3, 3];
+        public int[,] correctMatrix = new int[3, 3] { {1, 2, 3 }, { 8, 0, 4 },
+                                        {7,6, 5} };
+        public Graph newGraph;
+        public void PrintPath(GraphNode node)
+        {
+            int counter = 0;
+            while (node != null)
+            {
+                printNode(node);
+                node = node.backElement;
+                counter++;
+            }
+            Console.WriteLine("Total nodes: " + counter);
+        }
+        public void printNode(GraphNode node)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(node.NumbersArray[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+        
+        public bool compareArrays(int[,] arrayA, int[,] arrayB)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (arrayA[i, j] != arrayB[i, j])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        public void MoveUp(GraphNode node, int row, int line)
+        {
+            int[,] matrix = node.GetNodeMatrix();
+            int[,] tempMatrix = (int[,])matrix.Clone();
+            tempMatrix[row, line] = tempMatrix[row, line - 1];
+            tempMatrix[row, line - 1] = 0;
+            GraphNode newItem = new GraphNode(tempMatrix);
+            if (nodesList.FirstOrDefault(x => compareArrays(x.NumbersArray, tempMatrix)) == null)
+            {
+                newGraph.addNode(newItem);
+                newGraph.addEdge(node, newItem);
+                nodesList.Add(newItem);
+            }
+
+
+        }
+        public void MoveDown(GraphNode node, int row, int line)
+        {
+            int[,] matrix = node.GetNodeMatrix();
+            int[,] tempMatrix = (int[,])matrix.Clone();
+            tempMatrix[row, line] = tempMatrix[row, line + 1];
+            tempMatrix[row, line + 1] = 0;
+            GraphNode newItem = new GraphNode(tempMatrix);
+            if (nodesList.FirstOrDefault(x => compareArrays(x.NumbersArray, tempMatrix)) == null)
+            {
+                newGraph.addNode(newItem);
+                newGraph.addEdge(node, newItem);
+                nodesList.Add(newItem);
+            }
+
+        }
+        public void MoveLeft(GraphNode node, int row, int line)
+        {
+            int[,] matrix = node.GetNodeMatrix();
+            int[,] tempMatrix = (int[,])matrix.Clone();
+            tempMatrix[row, line] = tempMatrix[row - 1, line];
+            tempMatrix[row - 1, line] = 0;
+            GraphNode newItem = new GraphNode(tempMatrix);
+            if (nodesList.FirstOrDefault(x => compareArrays(x.NumbersArray, tempMatrix)) == null)
+            {
+                newGraph.addNode(newItem);
+                newGraph.addEdge(node, newItem);
+                nodesList.Add(newItem);
+            }
+        }
+        public void MoveRight(GraphNode node, int row, int line)
+        {
+            int[,] matrix = node.GetNodeMatrix();
+            int[,] tempMatrix = (int[,])matrix.Clone();
+            tempMatrix[row, line] = tempMatrix[row + 1, line];
+            tempMatrix[row + 1, line] = 0;
+            GraphNode newItem = new GraphNode(tempMatrix);
+            if (nodesList.FirstOrDefault(x => compareArrays(x.NumbersArray, tempMatrix)) == null)
+            {
+                newGraph.addNode(newItem);
+                newGraph.addEdge(node, newItem);
+                nodesList.Add(newItem);
+            }
+        }
+    }
+}
